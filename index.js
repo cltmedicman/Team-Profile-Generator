96fc7b4c235d98656dd2f125fb1519675ff18cba
chriscoding1 <cltmedicman@gmail.com>
2021-10-10 19:52:58 -0400

    Deleted commented out code from index.js and created genearteHtml.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+const generateHtml = require("./utils/generateHtml");
+
 let engineerArr = [];
 let internArr = [];
 let teamManager = [];
